136699f942ab02357117af57cbb069ca4698a888
jaimevalero <epgmad6.tid@telefonica.com>
2021-12-02 07:05:42 +0100

    refact: Preprocess

--- pandas_diff/pandas_diff.py
+++ pandas_diff/pandas_diff.py
@@ -1,56 +1,22 @@
 """Main module."""
 import pandas as pd
+from pre_process import pre_process
+#A = pd.DataFrame({'col_1': [1, 2, 3, 4, 5], 'col_2': [1, 2, 3, 4, 5]})
+#B = pd.DataFrame({'col_1': [1, 2, 3, 3, 5], 'col_3': [1, 2, 4, 5, 6]})
 
-def pre_process(df_before: pd.DataFrame , df_after: pd.DataFrame, compare_columns):
-    """
-    Pre-process the dataframe.
+A = pd.DataFrame([{"hero" : "hulk" , "power" : "strength"},
+                  {"hero" : "black_widow" , "power" : "spy"},
+                  {"hero" : "thor" , "hammers" : 0 },
+                  {"hero" : "thor" , "hammers" : 1 } ] )
+B = pd.DataFrame([{"hero" : "hulk" , "power" : "smart"},
+                 {"hero" : "captain marvel" , "power" : "strength"},
+                 {"hero" : "thor" , "hammers" : 2 } ] )
 
-    Parameters
-    ----------
-    df_before : pandas.DataFrame
-        The dataframe to pre-process.
-    df_after : pandas.DataFrame
-    Returns
-    -------
-    pandas.DataFrame
-        The pre-processed dataframe.
-    """
-    A,B = normalize_columns(df_before, df_after, compare_columns)
+#df =  pd.DataFrame({'col_1': [1, 2, 3, 3, 5], 'col_3': [1, 2, 4, 5, 6]})
+A,B , keys = pre_process(A, B, "hero")
 
+A["___keys"] = A[keys]
+B["___keys"] = B[keys]
 
-    # Remove the index column
-    #df = df.reset_index(drop=True)
+a=0
 
-    # Remove the column with the index
-    #df = df.drop(columns=['index'])
-
-    return
-
-def normalize_columns(df_before, df_after, compare_columns):
-    """ Ensure that the columns are the same in both df's. """
-    A = df_before.copy()
-    B = df_after.copy()
-
-    keys = []
-    if isinstance(compare_columns, str):
-        keys.append(compare_columns)
-    elif isinstance(compare_columns, list):
-        keys = compare_columns
-
-    # Check if the dataframes have the same columns.
-    are_columns_different = not A.columns.equals(B.columns)
-    if are_columns_different:
-        columns_only_in_A = list(set(A.columns) - set(B.columns))
-        columns_only_in_B = list(set(B.columns) - set(A.columns))
-
-        for c in columns_only_in_A:
-            B[c] = None
-        for c in columns_only_in_B:
-            A[c] = None
-
-    return A,B
-
-A = pd.DataFrame({'A': [1, 2, 3, 4, 5], 'B': [1, 2, 3, 4, 5]})
-B = pd.DataFrame({'A': [1, 2, 3, 4, 5], 'C': [1, 2, 4, 4, 5]})
-
-pre_process(A, B, ['A'])
---
+++ pandas_diff/pre_process.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import warnings
+
+def pre_process(df_before: pd.DataFrame , df_after: pd.DataFrame, compare_columns):
+    """
+    Pre-process the dataframe.
+
+    Parameters
+    ----------
+    df_before : pandas.DataFrame
+        The dataframe to pre-process.
+    df_after : pandas.DataFrame
+    Returns
+    -------
+    pandas.DataFrame
+        The pre-processed dataframe.
+    """
+    keys = normalize_keys(compare_columns)
+    A,B = normalize_columns(df_before, df_after)
+
+    A = remove_dupes(compare_columns, A, "before")
+    B = remove_dupes(compare_columns, B, "after")
+
+    return A,B, keys
+
+def remove_dupes(compare_columns, df, name):
+    """ Check if the dataframe has duplicate rows. And remove them. """
+    duplicated_rows_in_df = df[df.duplicated(subset=compare_columns, keep=False)].shape[0]
+    if duplicated_rows_in_df  :
+        duplicated = str(df[df.duplicated(subset=compare_columns, keep=False)].to_dict(orient="records"))
+        message_warning = f"Found {duplicated_rows_in_df} duplicated rows in {name} dataframe: " + duplicated + ". Removing duplicates."
+        warnings.warn(message_warning)
+        df = df.drop_duplicates(subset=compare_columns, keep="last")
+    return df
+
+def normalize_columns(df_before : pd.DataFrame, df_after: pd.DataFrame) -> list:
+    """ Ensure that the columns are the same in both df's. """
+    A = df_before.copy()
+    B = df_after.copy()
+
+
+
+    # Check if the dataframes have the same columns.
+    are_columns_different = not A.columns.equals(B.columns)
+    if are_columns_different:
+        columns_only_in_A = list(set(A.columns) - set(B.columns))
+        columns_only_in_B = list(set(B.columns) - set(A.columns))
+
+        for c in columns_only_in_A:
+            B[c] = None
+        for c in columns_only_in_B:
+            A[c] = None
+
+    return A,B
+
+def normalize_keys(compare_columns) -> list :
+    keys = []
+    if isinstance(compare_columns, str):
+        keys.append(compare_columns)
+    elif isinstance(compare_columns, list):
+        keys = compare_columns
+    return keys
+
+
