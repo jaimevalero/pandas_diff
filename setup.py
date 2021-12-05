8c1a308457033d7d14105aab313296391256bd9b
jaimevalero <jaimevalero78@yahoo.es>
2021-12-05 13:24:19 +0100

    Bump version: 0.7.0 → 0.7.1

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/jaimevalero/pandas_diff',
-    version='0.7.0',
+    version='0.7.1',
     zip_safe=False,
 )
