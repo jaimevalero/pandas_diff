f9526c7f4d1aa915bf14cc6400692b2d641a28a0
jaimevalero <jaimevalero78@yahoo.es>
2021-12-05 16:16:31 +0100

    bump version

--- HISTORY.rst
+++ HISTORY.rst
@@ -41,3 +41,8 @@ History
 
 \* bump version 
 
+0.7.13 (2021-12-05)
+-------------------
+
+\* bump version 
+
--- pandas_diff/__init__.py
+++ pandas_diff/__init__.py
@@ -4,6 +4,5 @@ __author__ = """Jaime Valero"""
 __email__ = 'jaimevalero78@gmail.com'
 __version__ = '0.7.13'
 
-import pandas as pd
 
 from pandas_diff.pandas_diff import get_diffs
